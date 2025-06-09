60923df78c110561999186332f1b8b421b397d93
shubham goyal <shubhamgoyal.dev@gmail.com>
2025-06-09 20:56:56 +0530

    main fixed

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from dotenv import load_dotenv
 from worker.worker import queue_reader
 load_dotenv()
 from helpers.server import app
+import uvicorn
 
 
+def main():
+    # queue_reader()
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
-# def main():
-#     # queue_reader()
-#     # uvicorn.run("helpers.server:app", host="0.0.0.0", port=8000, reload=True)
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
     
